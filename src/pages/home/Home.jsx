6f8aad9482537588ca49548dc98f26681d8a07e4
shabeebleo <shabeebpv0786@gmail.com>
2023-01-26 15:00:03 +0530

    audit fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import PostSide from "../../components/PostSide/PostSide";
-import ProfileSide from "../../components/profileSide/ProfileSide";
+import ProfileSide from "../../components/ProfileSide/ProfileSide";
 import RightSide from "../../components/RightSide/RightSide";
 import "./Home.css";
 
